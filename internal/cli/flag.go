4d32e4ac2b9921ac50abbe07a2784f490c2046c8
bear <yangyang.tian@ttpai.cn>
2025-03-28 18:16:58 +0800

    v1.1.2(优化配置文件读取)

--- internal/cli/flag.go
+++ internal/cli/flag.go
@@ -2,13 +2,23 @@ package cli
 
 import (
 	"flag"
+	"os"
+	"path/filepath"
 )
 
 // ConfigFilePath 默认读取的配置文件路径
 var ConfigFilePath = "default.yaml"
 
 func Init() {
+	// 获取当前执行文件的路径
+	execPath, err := os.Executable()
+	if err != nil {
+		panic(err)
+	}
+	execDir := filepath.Dir(execPath)
+
 	// 自定义配置文件路径，会覆盖默认的配置
-	flag.StringVar(&ConfigFilePath, "config", "config/default.yaml", "配置文件路径")
+	defaultConfigPath := filepath.Join(execDir, "config/default.yaml")
+	flag.StringVar(&ConfigFilePath, "config", defaultConfigPath, "配置文件路径")
 	flag.Parse()
 }
