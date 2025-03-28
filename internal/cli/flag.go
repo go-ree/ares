c82bdf6e28d1818e85e7c7182783d821efc1f572
bear <yangyang.tian@ttpai.cn>
2025-03-28 16:49:58 +0800

    v1.1.0(添加忽略文件)

--- internal/cli/flag.go
+++ internal/cli/flag.go
@@ -5,10 +5,10 @@ import (
 )
 
 // ConfigFilePath 默认读取的配置文件路径
-var ConfigFilePath = "config.yaml"
+var ConfigFilePath = "default.yaml"
 
 func Init() {
 	// 自定义配置文件路径，会覆盖默认的配置
-	flag.StringVar(&ConfigFilePath, "config", "configs/config.yaml", "配置文件路径")
+	flag.StringVar(&ConfigFilePath, "config", "config/default.yaml", "配置文件路径")
 	flag.Parse()
 }
