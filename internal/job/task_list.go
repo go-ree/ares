21203ade70de66c61f643796f1b191e417ec47f5
bear <yangyang.tian@ttpai.cn>
2025-04-02 14:09:58 +0800

    v1.2.5(优化发布任务列表获取逻辑，仅获取最近3小时内的任务。优化定时任务job的执行逻辑)

--- internal/job/task_list.go
+++ internal/job/task_list.go
@@ -1,16 +1,10 @@
 package job
 
 import (
-	"ares/internal/home"
 	"ares/internal/publish"
 )
 
 func init() {
 	task := publish.NewTaskManager()
-	Register("aaa", aaa)
 	Register("taskManager", task.UpdateTaskStatuses)
-}
-
-func aaa() {
-	home.HomeJob()
 }
--- internal/publish/publish.go
+++ internal/publish/publish.go
@@ -307,9 +307,23 @@ func (pm *PublishManager) ComposePublishData(req *PublishRequest, app *entity.Ap
 
 func (pm *PublishManager) JobStatus() ([]*entity.TaskRecord, error) {
 	var taskRecords []*entity.TaskRecord
-	err := db.Engine.Where("status IN (?, ?, ?)", "init", "packaging", "deploying").Find(&taskRecords)
+
+	// 计算3小时前的时间点
+	threeHoursAgo := time.Now().Add(-3 * time.Hour)
+
+	err := db.Engine.
+		Where("status IN (?, ?, ?)", "init", "packaging", "deploying").
+		And("created_at > ?", threeHoursAgo).
+		Find(&taskRecords)
+
 	if err != nil {
 		return nil, fmt.Errorf("查询任务状态失败：%s", err)
 	}
+
+	// 添加日志记录
+	slog.Info("查询任务状态",
+		"count", len(taskRecords),
+		"time_range", fmt.Sprintf("获取最近3小时数据，应用状态为init、packaging、deploying的数据(%s)", threeHoursAgo.Format("2006-01-02 15:04:05")))
+
 	return taskRecords, nil
 }
