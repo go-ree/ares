c8cb12761ab9af9f7eafbf31ee147d1656f4493a
bear <yangyang.tian@ttpai.cn>
2025-06-09 18:24:12 +0800

    v0.2.2（优化入口容易造成歧义的bug）

--- frontend/app/web/main.ts
+++ frontend/app/web/main.ts
@@ -1,14 +1,18 @@
 import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import '../../src/style.css'
 import App from './App.vue'
+import zhCn from "element-plus/es/locale/lang/zh-cn"
 import router from './routes'
 
 // 创建应用实例
 const app = createApp(App)
 
 // 使用插件
-app.use(ElementPlus)
+app.use(ElementPlus, {
+    locale: zhCn,
+})
 app.use(router)
 
 // 挂载应用
